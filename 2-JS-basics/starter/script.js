df25fdc38e2dae1c208ce578185d0f039065b013
panuwat33586 <panuwat33586@gmail.com>
2020-03-03 15:55:58 +0700

    JS Operator

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,4 +1,4 @@
-/*
+/********************************** 
 *Variable and data types
 */
 var firstName='John';
@@ -22,7 +22,7 @@ var _3years=3; //variable name must be string,$ or_ cannot use reserved keyword 
 
 var johnMark='John and MArk';
 
-/*
+/********************************** 
 *Variable mutation and type correction
 */
 
@@ -41,3 +41,43 @@ alert(firstName+' '+'is a'+' '+job);
 
 var job=prompt('What is his job?');
 console.log(job);
+
+/********************************** 
+*Basic operators
+*/
+var year,yearJohn,yearMark;
+now =2018;
+ageJohn=28;
+ageMark=33;
+
+//Math operator
+yearJohn=now-ageJohn;
+yearMark=now-ageMark;
+
+console.log(yearJohn);
+
+console.log(now+2);
+console.log(now*2);
+console.log(now/10);
+
+//logical operators
+var johnOlder=ageJohn<ageMark
+console.log(johnOlder)
+
+//typeof operator
+console.log(typeof johnOlder)
+console.log(typeof ageJohn)
+console.log(typeof 'Mark is older than john')
+var x
+console.log(typeof x)
+
+/********************************** 
+*Operator precendence(which opearator proceeed first )
+*/
+
+var now =2018;
+var yaerJohn =1989;
+var fullAge=18;
+
+var isFullAge=now-yearJohn >=fullAge
+console.log(isFullAge)
