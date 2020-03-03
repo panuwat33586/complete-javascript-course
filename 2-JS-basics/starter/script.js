42e5663f903fe8a4e52b9a6671960427551c5536
panuwat33586 <panuwat33586@gmail.com>
2020-03-03 14:26:05 +0700

    learning JS Type Corecion and Data type

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,3 +1,6 @@
+/*
+*Variable and data types
+*/
 var firstName='John';
 console.log(firstName);
 
@@ -8,3 +11,33 @@ var fullAge=true;
 
 console.log(fullAge);
 
+var job;
+console.log(job);
+
+job='Teacher';
+
+console.log(job);
+
+var _3years=3; //variable name must be string,$ or_ cannot use reserved keyword as varaible name 
+
+var johnMark='John and MArk';
+
+/*
+*Variable mutation and type correction
+*/
+
+// type coercion
+var firstName='John';
+var age=28
+
+console.log(firstName+' '+age); // Unlike another programming language JS auto convert Int to String or even boolean (type coercion) 
+
+// variable mutation 
+
+age='twenty eight';
+job ='driver';
+
+alert(firstName+' '+'is a'+' '+job);
+
+var job=prompt('What is his job?');
+console.log(job);
